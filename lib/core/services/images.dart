da40dd248aea551c18a7fb6404008efb76f85fa9
neeraj22003 <neerajsharma5370@gmail.com>
2026-03-28 14:54:10 +0530

    refactor: centralize image asset paths in ImageService

---
+++ lib/core/services/images.dart
@@ -0,0 +1,14 @@
+class ImageService {
+  static String ast = 'assets';
+  static String get appImage => '$ast/app.png';
+  static String get chatBg => '$ast/bg.jp';
+  static String get email => '$ast/email.png';
+  static String get google => '$ast/google.png';
+  static String get gray => '$ast/gray.png';
+  static String get header => '$ast/header.jpg';
+  static String get login => '$ast/login.png';
+  static String get noOrders => '$ast/noorders.png';
+  static String get placeholder => '$ast/placeholder.png';
+  static String get searchUser => '$ast/searchUser.png';
+  static String get widelogin => '$ast/widelogin.jpg';
+}
